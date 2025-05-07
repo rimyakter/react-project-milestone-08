0d658e078221f25068695357ed4887fbe6507805
Rimy Akter <rimy@gmail.com>
2025-05-07 12:14:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "./Link";
+
+const navigationData = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Services", path: "/services" },
+  { id: 4, name: "Blog", path: "/blog" },
+  { id: 5, name: "Contact", path: "/contact" },
+];
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="flex">
+        {navigationData.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
